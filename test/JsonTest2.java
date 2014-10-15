8d84062bfa12a19734e51f1852a1e89c7901072c
SagehariMaiko <sagehari_m@welmo.co.jp>
2014-10-15 13:43:53 +0900

    JSONからdatファイル作成

--- test/JsonTest2.java
+++ test/JsonTest2.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.*;
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.FileWriter;
@@ -10,40 +12,56 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JsonTest2 {
 
-	private final static String outputDatFile = "C:\\Users\\Sagehari Maiko\\Documents\\jsontest\\home_care.dat";	// change pass
-	private final static String inputDatFile = "C:\\Users\\Sagehari Maiko\\Documents\\jsontest\\sampleheader.dat";
+	private final static String outputDatFile = "C:\\Users\\Sagehari Maiko\\Documents\\jsontest\\110.dat";	// change pass
+	private final static String inputDatFile = "C:\\Users\\Sagehari Maiko\\Documents\\jsontest\\110.csv";	// change pass
 	
-    public static void main(String args[]) throws JsonParseException,
-            JsonMappingException, IOException {
-        JsonTest2 converter = new JsonTest2();
-        converter.parse("sample3.json");
-    }
-    public void parse(String fname) throws JsonParseException,
-            JsonMappingException, IOException {
-        List<Data> offices = new ObjectMapper().readValue(new File(fname),
-                new TypeReference<List<Data>>() {
-                });
-                
-        File outputFile = new File(outputDatFile);
-        File inputFile = new File(inputDatFile);
-        
-        BufferedWriter out = null;
-        BufferedReader in = null;
-        
-        try{
-        	in = new BufferedReader(new FileReader(inputFile));
-	        out = new BufferedWriter(new FileWriter(outputFile));
+	public static void main(String args[]) throws JsonParseException,JsonMappingException, IOException {
+		JsonTest2 converter = new JsonTest2();
+		converter.parse("sample3.json");	// change filename
+	}
+
+	public void parse(String fname) throws JsonParseException,JsonMappingException, IOException {
+		List<Map<String,String>> offices = new ObjectMapper().readValue(new File(fname),new TypeReference<List<Map<String,String>>>() {});
+
+		File outputFile = new File(outputDatFile);
+		File inputFile = new File(inputDatFile);
+
+		BufferedWriter out = null;
+		BufferedReader in = null;
+
+		String[] csvArray;
+		int count = 0;
+		String line;
+		int csvReadStart = 5;
+
+		List<String> ColList = new ArrayList<String>();
+
+		try{
+			in = new BufferedReader(new FileReader(inputFile));
+			out = new BufferedWriter(new FileWriter(outputFile));
 	        
-	        out.write(in.readLine());
-	        out.newLine();
-	        
-	        for (Data c : offices) {
-	            //System.out.println(c.toString());
-	            out.write(c.toString());
-	            out.newLine();
-	        }
+			while ((line = in.readLine()) != null){
+				csvArray = line.split("\\,");
+				count++;
+				if(count >= csvReadStart && csvArray.length >= 10){
+					System.out.println(csvArray[1]);
+					if( count != csvReadStart ){out.write(",");}
+					out.write(csvArray[1]);
+					ColList.add(csvArray[1]);
+				}
+			}
+			out.newLine();
+
+			for (Map<String,String> c : offices) {
+				for ( int i = 0; i < ColList.size(); ++i ) {
+					if( i != 0 ){out.write(",");}
+					out.write((c.get(ColList.get(i))).replaceAll(",",""));
+				}
+				out.newLine();
+			}
 		}catch(FileNotFoundException e){
 			e.printStackTrace();
 		}catch(IOException e){
@@ -62,7 +80,7 @@ public class JsonTest2 {
 			}
 		}
     }
-    public static class Data {
+/*    public static class Data {
         private String company_type;
         private String office_time_hol;
         private String office_time_sun;
@@ -97,5 +115,5 @@ public class JsonTest2 {
         public String toString() {
             return company_type+","+office_time_hol+","+office_time_sun;
         }
-    }
+    }*/
 }
