843abb4cafa0d571d2a54d9d739a8a28bf3c00d6
Laurie <laurie_g@welmo.co.jp>
2014-10-29 15:52:02 +0900

    Simplify scrape all batch script

--- scraper/scrapeAll.bat
+++ scraper/scrapeAll.bat
@@ -1,46 +1,50 @@
 @echo off
+scrapy crawl basicSpider -a serviceType=110
+ren utf8output.json 110.json
+scrapy crawl basicSpider -a serviceType=120
+ren utf8output.json 120.json
 scrapy crawl basicSpider -a serviceType=130
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 130.json
+ren utf8output.json 130.json
 scrapy crawl basicSpider -a serviceType=140
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 140.json
+ren utf8output.json 140.json
 scrapy crawl basicSpider -a serviceType=170
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 170.json
+ren utf8output.json 170.json
 scrapy crawl basicSpider -a serviceType=210
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 210.json
+ren utf8output.json 210.json
 scrapy crawl basicSpider -a serviceType=220
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 220.json
+ren utf8output.json 220.json
 scrapy crawl basicSpider -a serviceType=230
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 230.json
+ren utf8output.json 230.json
 scrapy crawl basicSpider -a serviceType=320
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 320.json
+ren utf8output.json 320.json
 scrapy crawl basicSpider -a serviceType=331
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 331.json
+ren utf8output.json 331.json
 scrapy crawl basicSpider -a serviceType=331A
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 331A.json
+ren utf8output.json 331A.json
 scrapy crawl basicSpider -a serviceType=332
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 332.json
+ren utf8output.json 332.json
 scrapy crawl basicSpider -a serviceType=334
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 334.json
+ren utf8output.json 334.json
 scrapy crawl basicSpider -a serviceType=361
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 361.json
+ren utf8output.json 361.json
 scrapy crawl basicSpider -a serviceType=362
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 362.json
+ren utf8output.json 362.json
 scrapy crawl basicSpider -a serviceType=410
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 410.json
+ren utf8output.json 410.json
 scrapy crawl basicSpider -a serviceType=510
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 510.json
+ren utf8output.json 510.json
 scrapy crawl basicSpider -a serviceType=520
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 520.json
+ren utf8output.json 520.json
 scrapy crawl basicSpider -a serviceType=530
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 530.json
+ren utf8output.json 530.json
 scrapy crawl basicSpider -a serviceType=540
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 540.json
+ren utf8output.json 540.json
 scrapy crawl basicSpider -a serviceType=710
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 710.json
+ren utf8output.json 710.json
 scrapy crawl basicSpider -a serviceType=730
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 730.json
+ren utf8output.json 730.json
 scrapy crawl basicSpider -a serviceType=760
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 760.json
+ren utf8output.json 760.json
 scrapy crawl basicSpider -a serviceType=770
-ren C:\Users\laurie-account\gitworkspace\sandbox\scraper\utf8output.json 770.json
+ren utf8output.json 770.json
 pause
