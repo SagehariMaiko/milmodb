4cc2b9d4700786bb581f540c04c5e6bb19596b0e
SagehariMaiko <sagehari_m@welmo.co.jp>
2014-11-27 19:52:51 +0900

    NewwarTest #41

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -5,13 +5,7 @@ import play.mvc.Http.*;
 import models.*;
 
 public class ApplicationTest extends FunctionalTest {
+	
 
-    @Test
-    public void testThatIndexPageWorks() {
-        Response response = GET("/");
-        assertIsOk(response);
-        assertContentType("text/html", response);
-        assertCharset(play.Play.defaultWebEncoding, response);
-    }
     
 }
--- test/BasicTest.java
+++ test/BasicTest.java
@@ -4,10 +4,5 @@ import play.test.*;
 import models.*;
 
 public class BasicTest extends UnitTest {
-
-    @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
-    }
-
+	
 }
